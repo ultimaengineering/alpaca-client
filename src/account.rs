3333cef4d30d99d9b5face572a44c88ebaf99563
Alexander Montgomery <evaderxander@gmail.com>
2020-05-27 23:51:04 -0400

    Adding comments for struct description

--- src/account.rs
+++ src/account.rs
@@ -5,30 +5,37 @@ use serde::{Serialize, Deserialize};
 // https://alpaca.markets/docs/api-documentation/api-v2/account/
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Account {
-    pub account_blocked: String,
-    pub account_number: String,
-    pub buying_power: Decimal,
-    pub cash: Decimal,
-    pub created_at: DateTime<Utc>,
-    pub currency: String,
-    pub daytrade_count: i32,
+    pub account_blocked: String, //If true, the account activity by user is prohibited.
+    pub account_number: String, //Account number.
+    pub buying_power: Decimal, //Current available $ buying power; If multiplier = 4,
+    // this is your daytrade buying power which is calculated as (last_equity - (last) maintenance_margin) * 4;
+    // If multiplier = 2, buying_power = max(equity – initial_margin,0) * 2;
+    // If multiplier = 1, buying_power = cash
+    pub cash: Decimal, //Cash balance
+    pub created_at: DateTime<Utc>, //Timestamp this account was created at
+    pub currency: String, //“USD”
+    pub daytrade_count: i32, //The current number of daytrades that have been made in the last 5 trading days (inclusive of today)
     pub daytrading_buying_power: Decimal,
-    pub equity: Decimal,
-    pub id: uuid::Uuid,
+    pub equity: Decimal, //Cash + long_market_value + short_market_value
+    pub id: uuid::Uuid, //Account ID.
     pub initial_margin: Decimal,
-    pub last_equity: Decimal,
+    pub last_equity: Decimal, //Equity as of previous trading day at 16:00:00 ET
     pub last_maintenance_margin: Decimal,
-    pub long_market_value: Decimal,
+    pub long_market_value: Decimal, //Real-time MtM value of all long positions held in the account
     pub maintenance_margin: Decimal,
-    pub multiplier: String,
-    pub pattern_day_trader: String,
-    pub portfolio_value: Decimal,
+    pub multiplier: String, //Buying power multiplier that represents account margin classification;
+    // valid values 1 (standard limited margin account with 1x buying power),
+    // 2 (reg T margin account with 2x intraday and overnight buying power;
+    // this is the default for all non-PDT accounts with $2,000 or more equity),
+    // 4 (PDT account with 4x intraday buying power and 2x reg T overnight buying power)
+    pub pattern_day_trader: String, //Whether or not the account has been flagged as a pattern day trader
+    pub portfolio_value: Decimal, //Total value of cash + holding positions (This field is deprecated. It is equivalent to the equity field.)
     pub regt_buying_power: Decimal,
-    pub short_market_value: Decimal,
-    pub shorting_enabled: String,
-    pub sma: String,
-    pub status: String,
-    pub trade_suspended_by_user: String,
-    pub trading_blocked: String,
-    pub transfers_blocked: String
+    pub short_market_value: Decimal, //Real-time MtM value of all short positions held in the account
+    pub shorting_enabled: String, //Flag to denote whether or not the account is permitted to short
+    pub sma: String, //Value of special memorandum account (will be used at a later date to provide additional buying_power)
+    pub status: String, //See Account Status
+    pub trade_suspended_by_user: String, //User setting. If true, the account is not allowed to place orders.
+    pub trading_blocked: String, //If true, the account is not allowed to place orders.
+    pub transfers_blocked: String //If true, the account is not allowed to request money transfers.
 }
--- src/order.rs
+++ src/order.rs
@@ -4,8 +4,8 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Order {
-    pub id: uuid::Uuid,
-    pub client_order_id: uuid::Uuid,
+    pub id: uuid::Uuid, //Order id
+    pub client_order_id: uuid::Uuid, //Client unique order id
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
     pub submitted_at: DateTime<Utc>,
@@ -14,18 +14,19 @@ pub struct Order {
     pub canceled_at: DateTime<Utc>,
     pub failed_at: DateTime<Utc>,
     pub replaced_at: DateTime<Utc>,
-    pub replaces: Option<uuid::Uuid>,
-    pub asset_id: uuid::Uuid,
-    pub symbol: String,
-    pub asset_class: String,
-    pub qty: String,
-    pub filled_qty: String,
-    pub side: String,
-    pub time_in_force: String,
-    pub limit_price: Decimal,
-    pub stop_price: Decimal,
-    pub filled_avg_price: Decimal,
-    pub status: String,
-    pub extended_hours: bool,
-    pub legs: Option<String>
+    pub replaces: Option<uuid::Uuid>,// The order ID that this order replaces
+    pub asset_id: uuid::Uuid, // Asset ID
+    pub symbol: String, // Asset symbol
+    pub asset_class: String, // Asset class
+    pub qty: String, // Ordered quantity
+    pub filled_qty: String, // Filled quantity
+    pub side: String, //Valid values: buy, sell
+    pub time_in_force: String, //https://alpaca.markets/docs/trading-on-alpaca/orders/#time-in-force
+    pub limit_price: Decimal, //Limit price
+    pub stop_price: Decimal, //Stop price
+    pub filled_avg_price: Decimal, //Filled average price
+    pub status: String, //
+    pub extended_hours: bool, //If true, eligible for execution outside regular trading hours.
+    pub legs: Option<String> //When querying non-simple order_class orders in a nested style,
+    // an array of Order entities associated with this order. Otherwise, null.
 }
