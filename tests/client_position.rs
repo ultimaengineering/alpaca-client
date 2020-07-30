8618acef77fede2a9881437ca625ea267cad21d4
Alexander Montgomery <evaderxander@gmail.com>
2020-07-30 15:14:34 -0400

    Adding position tests.

--- tests/client_position.rs
+++ tests/client_position.rs
@@ -7,25 +7,162 @@ mod tests {
     use alpaca::client::AccountType::PAPER;
     use std::process::exit;
     use std::borrow::Borrow;
+    use alpaca::order::Order;
+    use uuid::Uuid;
+    use alpaca::position::Position;
 
     #[test]
     pub fn get_open_positions() {
-
+        let client = get_client();
+        let new_order = Order {
+            id: Some(Uuid::new_v4()),
+            client_order_id: Uuid::new_v4(),
+            created_at: None,
+            updated_at:  None,
+            submitted_at:  None,
+            filled_at:  None,
+            expired_at:  None,
+            canceled_at:  None,
+            failed_at: None,
+            replaced_at: None,
+            replaces: None,
+            asset_id: Default::default(),
+            symbol: "NVDA".to_string(),
+            asset_class: "".to_string(),
+            qty: "100".to_string(),
+            filled_qty: "".to_string(),
+            side: "buy".to_string(),
+            order_type: "stop_limit".parse().unwrap(),
+            time_in_force: "day".to_string(),
+            limit_price: Some("1000".parse().unwrap()),
+            stop_price: Some("1".parse().unwrap()),
+            filled_avg_price: Default::default(),
+            status: "".to_string(),
+            extended_hours: false,
+            legs: None
+        };
+        &client.place_order(new_order.clone());
+        let positions:Vec<Position> = client.get_open_positions();
+        let result = positions.into_iter()
+            .any(|x| x.symbol.eq(&new_order.borrow().symbol));
+        assert!(result, true)
     }
 
     #[test]
     pub fn get_open_position() {
-
+        let client = get_client();
+        let new_order = Order {
+            id: Some(Uuid::new_v4()),
+            client_order_id: Uuid::new_v4(),
+            created_at: None,
+            updated_at:  None,
+            submitted_at:  None,
+            filled_at:  None,
+            expired_at:  None,
+            canceled_at:  None,
+            failed_at: None,
+            replaced_at: None,
+            replaces: None,
+            asset_id: Default::default(),
+            symbol: "AMD".to_string(),
+            asset_class: "".to_string(),
+            qty: "100".to_string(),
+            filled_qty: "".to_string(),
+            side: "buy".to_string(),
+            order_type: "stop_limit".parse().unwrap(),
+            time_in_force: "day".to_string(),
+            limit_price: Some("1000".parse().unwrap()),
+            stop_price: Some("1".parse().unwrap()),
+            filled_avg_price: Default::default(),
+            status: "".to_string(),
+            extended_hours: false,
+            legs: None
+        };
+        &client.place_order(new_order.clone());
+        let mut positions = Vec::new();
+        positions.push(client.get_open_position("AMD".to_string()));
+        let result = positions.into_iter()
+            .any(|x : Position | x.symbol.eq(&new_order.borrow().symbol));
+        assert!(result, true)
     }
 
     #[test]
     pub fn close_all_positions() {
-
+        let client = get_client();
+        let new_order = Order {
+            id: Some(Uuid::new_v4()),
+            client_order_id: Uuid::new_v4(),
+            created_at: None,
+            updated_at:  None,
+            submitted_at:  None,
+            filled_at:  None,
+            expired_at:  None,
+            canceled_at:  None,
+            failed_at: None,
+            replaced_at: None,
+            replaces: None,
+            asset_id: Default::default(),
+            symbol: "INTC".to_string(),
+            asset_class: "".to_string(),
+            qty: "100".to_string(),
+            filled_qty: "".to_string(),
+            side: "buy".to_string(),
+            order_type: "stop_limit".parse().unwrap(),
+            time_in_force: "day".to_string(),
+            limit_price: Some("1000".parse().unwrap()),
+            stop_price: Some("1".parse().unwrap()),
+            filled_avg_price: Default::default(),
+            status: "".to_string(),
+            extended_hours: false,
+            legs: None
+        };
+        &client.place_order(new_order.clone());
+        let positions:Vec<Position> = client.get_open_positions();
+        assert!(positions.len() > 0);
+        client.close_all_positions();
+        let result = positions.into_iter()
+            .any(|x| x.symbol.eq(&new_order.borrow().symbol));
+        assert!(result, false);
+        assert!(client.get_open_positions().len() == 0)
     }
 
     #[test]
     pub fn close_position() {
-
+        let client = get_client();
+        let new_order = Order {
+            id: Some(Uuid::new_v4()),
+            client_order_id: Uuid::new_v4(),
+            created_at: None,
+            updated_at:  None,
+            submitted_at:  None,
+            filled_at:  None,
+            expired_at:  None,
+            canceled_at:  None,
+            failed_at: None,
+            replaced_at: None,
+            replaces: None,
+            asset_id: Default::default(),
+            symbol: "INTC".to_string(),
+            asset_class: "".to_string(),
+            qty: "100".to_string(),
+            filled_qty: "".to_string(),
+            side: "buy".to_string(),
+            order_type: "stop_limit".parse().unwrap(),
+            time_in_force: "day".to_string(),
+            limit_price: Some("1000".parse().unwrap()),
+            stop_price: Some("1".parse().unwrap()),
+            filled_avg_price: Default::default(),
+            status: "".to_string(),
+            extended_hours: false,
+            legs: None
+        };
+        &client.place_order(new_order.clone());
+        let positions:Vec<Position> = client.get_open_positions();
+        assert!(positions.len() > 0);
+        client.close_position("INTC".to_string());
+        let result = positions.into_iter()
+            .any(|x| x.symbol.eq(&new_order.borrow().symbol));
+        assert!(result, false)
     }
 
 
